c514141908f44b5b2c3f4269d44221b3399fde39
TheNotary <wastyx@gmail.com>
2011-11-13 10:14:05 -0600

    updated install instructions

--- install.bat
+++ install.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 REM Before running, make sure you run "gem install win32-service"
 REM Also, make sure you run it as administrator
+REM
+REM Also, don't forget to change the target server and port if it changes.
 pause
 
 ruby -v|findstr /r "ruby" >nul
