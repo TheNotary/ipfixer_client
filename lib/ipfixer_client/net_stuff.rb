853267d673680daea9f8f6b965ff417275f1ee99
N-jax <no.email@plz.com>
2013-01-21 15:36:14 -0600

    fixed bug where security token wasn't encapsulated in quotes causing the json request to be invalid.

--- lib/ipfixer_client/net_stuff.rb
+++ lib/ipfixer_client/net_stuff.rb
@@ -41,7 +41,7 @@ module IpfixerClient
     req.content_type = 'application/json'  # specify json
   
     #req.basic_auth username, password                          # TODO:  Sort this out down the road
-    json_string = "{ 'ipfix': { 'host': '#{host_name}', 'ip': '#{ip}', 'token': #{token} } }"
+    json_string = "{ 'ipfix': { 'host': '#{host_name}', 'ip': '#{ip}', 'token': '#{token}' } }"
     req.body = json_string.gsub("'", '"')        # json doesn't accept the single quote symbol
     
     
